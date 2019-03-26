98335b54bfb862ab454df0a016d9a706a987d040
Oleh Huzei <oleh.huzei@gmail.com>
2019-03-26 22:17:59 +0200

    Add pywebapp role to handle web-app installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from pyquestionnaire import __version__
+from setuptools import setup, find_packages
+
+setup(
+    name='pyquestionnaire',
+    packages=find_packages(),
+    include_package_data=True,
+    version=__version__,
+    install_requires=[]
+)
