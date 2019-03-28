b12455b58be316d7650f5853c5d69303d670be7f
Oleh Huzei <oleh.huzei@gmail.com>
2019-03-28 12:57:27 +0200

    Fix uWSGI <=> django communication

--- pyquestionnaire/settings.py
+++ pyquestionnaire/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pyquestionnaire.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ '/opt/pyquestionnaire/app/templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
